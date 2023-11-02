6230162d7d2ca56e82255e3e1c6045640c1224fc
KeerthanaC38 <keerthananakka38@gmail.com>
2023-11-02 02:11:18 -0500

    Final Changes

--- Enrollment.php
+++ Enrollment.php
@@ -8,6 +8,21 @@
         <label for="username">Username:</label>
         <input type="text" name="username" required><br>
 
+        <label for="address">Address:</label>
+        <input type="text" name="address" required><br>
+
+        <label for="city">City:</label>
+        <input type="text" name="city" required><br>
+
+        <label for="state">State:</label>
+        <input type="text" name="state" required><br>
+
+        <label for="zip">Zip:</label>
+        <input type="text" name="zip" required><br>
+
+        <label for="phone">Phone:</label>
+        <input type="text" name="phone" required><br>
+
         <label for="email">Email:</label>
         <input type="email" name="email" required><br>
 
